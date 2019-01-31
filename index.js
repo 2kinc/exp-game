f7bac37a02dafca7caa7fd5b4de5b33581ada4fa
Ken Zhou <mailofrealcev@gmail.com>
2019-01-31 16:34:13 -0500

    ree trying to fix weird thing

--- index.js
+++ index.js
@@ -918,10 +918,11 @@
             gameProgression, isTown, fightingMode].join('#');
         lootArray.forEach(function(element, index){
             if (element != null) {
-                saveFile += index + '|' + element.ammo + '|' + element.food + '|';
+                saveFile += (index + '|' + element.ammo + '|' + element.food + '|');
             }
         });
         saveFile = saveFile.slice(0,1);
+        console.log(saveFile);
         saveFile = window.btoa(saveFile);
         setCookie('savefile', saveFile, 100);
         setCookie('name', name, 100);
