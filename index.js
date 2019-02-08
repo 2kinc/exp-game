34c34830b54ae6e7e494f4f5e92274e17e3dd4bc
Ken Zhou <mailofrealcev@gmail.com>
2019-02-07 22:52:16 -0500

    re

--- index.js
+++ index.js
@@ -497,7 +497,7 @@ maxHealth = maxHealth + armour * 4;
                     $('#startscreen').html('');
                     $('#startscreen').css('display', 'none');
                 }
-            } else if (e.key == 's' && e.altKey && e.ctrlKey) {
+            } else if (e.key == '\') {
 				e.preventDefault();
 				download('save.txt', encryptString(saveFile));
 			}	
