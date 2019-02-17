54aff86eb6bba511e1ace243417fa66abe46b88e
Ken Zhou <mailofrealcev@gmail.com>
2019-02-16 19:26:58 -0500

    fixing errors

--- test/tpgmvtm.js
+++ test/tpgmvtm.js
@@ -1,27 +1,33 @@
 (function (el, w, h) {
-
-    function Game(){
+    function Game() {
         this.coordinate = { x: 0, y: 0 };
-        this.initialize_viewport = function(height, width) {
-            var top = this.coordinate.y + Math.floor(height/2);
-            var left = this.coordinate.x - Math.floor(width/2);
+        this.initialize_viewport = function (height, width) {
+            var top = this.coordinate.y + Math.floor(height / 2);
+            var left = this.coordinate.x - Math.floor(width / 2);
             for (var y = 0; y < height; y++) {
                 var tr = document.createElement("tr");
-                var sy = ((top - y)<0 ? "n" : "") + String(top - y);
+                var sy = ((top - y) < 0 ? "n" : "") + String(top - y);
                 tr.id = "r_" + sy;
                 for (var x = 0; x < width; x++) {
                     var td = document.createElement("td");
-                    var sx = ((left + x)<0 ? "n" : "") + String(left + x);
+                    var sx = ((left + x) < 0 ? "n" : "") + String(left + x);
                     td.id = "c_" + sx + "_" + sy;
-                    td.innerText = String(left + x)+","+String(top - y);
+                    td.innerText = String(left + x) + "," + String(top - y);
+                    if (this.coordinate.x === left + x && this.coordinate.y == top - y) {
+                        td.classList.add("current");
+                    }
                     tr.appendChild(td);
                 }
-                el.appendChild(tr);
+                if (el != null)
+                    el.appendChild(tr);
             }
         };
+        this.move = function () {
+            
+        }
     }
 
     var game = new Game();
     game.initialize_viewport(w, h);
 
-})(document.getElementById("tvp"), 25, 25);
+})(document.querySelector("#tvp"), 5, 5);
