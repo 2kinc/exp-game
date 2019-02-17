ad8ac37c4c1747553d83829981621d6b1fda5af9
Ken Zhou <mailofrealcev@gmail.com>
2019-02-16 22:15:33 -0500

    flying forward working now

--- test/tpgmvtm.js
+++ test/tpgmvtm.js
@@ -1,38 +1,77 @@
 (function (el, w, h) {
     function Game() {
         this.coordinate = { x: 0, y: 0 };
-        this.initialize_viewport = function (height, width) {
-            var top = this.coordinate.y + Math.floor(height / 2);
-            var left = this.coordinate.x - Math.floor(width / 2);
-            for (var y = 0; y < height; y++) {
-                var tr = document.createElement("tr");
-                var sy = ((top - y) < 0 ? "n" : "") + String(top - y);
-                tr.id = "r_" + sy;
-                for (var x = 0; x < width; x++) {
-                    var td = document.createElement("td");
-                    var sx = ((left + x) < 0 ? "n" : "") + String(left + x);
-                    td.id = "c_" + sx + "_" + sy;
-                    td.innerText = String(left + x) + "," + String(top - y);
-                    td.style.background = "rgb(" + Math.abs(left + x) * 8 % 256 + ',' + Math.abs(top - y) * 8 % 256 + ', 0)';
-                    if (this.coordinate.x === left + x && this.coordinate.y == top - y) {
-                        td.classList.add("current");
-                    }
-                    tr.appendChild(td);
-                }
-                if (el != null)
-                    el.appendChild(tr);
+        this.get_topleft = function () {
+            return {
+                y: this.coordinate.y + Math.floor(h / 2),
+                x: this.coordinate.x - Math.floor(w / 2)
             }
         };
-        this.shift_viewport_vertically = function(is_up, distance){
-            if(is_up){
-                this.coordinate += distance;
+        this.for_id = function (n) {
+            return String(Math.abs(n)) + (n < 0 ? "n" : "");
+        };
+        this.get_tile_id = function(sx, sy){
+            return "c_" + sx + "_" + sy;
+        };
+        this.generate_rows = function (topleft, n_rows, n_cols, start_index) {
+            var anchor = null;
+            if(start_index >= 0){
+                anchor = el.children[start_index];
             }
-            else{
-                this.coordinate -= distance;
+            for (var y = 0; y < n_rows; y++) {
+                var tr = document.createElement("tr");
+                var sy = this.for_id(topleft.y - y);
+                tr.id = "r_" + sy;
+                for (var x = 0; x < n_cols; x++) {
+                    var td = document.createElement("td");
+                    var sx = this.for_id(topleft.x + x);
+                    td.id = this.get_tile_id(sx, sy);
+                    td.innerText = String(topleft.x + x) + "," + String(topleft.y - y);
+                    td.style.background = "rgb(" + Math.abs(topleft.x + x) * 8 % 256 + ',' + Math.abs(topleft.y - y) * 8 % 256 + ', 0)';
+                    tr.appendChild(td);
+                }
+                if(anchor){
+                    el.insertBefore(tr, anchor);
+                }
+                else{
+                    el.appendChild(tr);
+                }
             }
-            /*
-            //to-do: remove bottom <tr>'s and insert new <tr>'s at the top            
-            */
+
+            var c = document.querySelector("td.current");
+            if(c) 
+            {
+                c.classList.remove("current");
+            }
+            c = document.getElementById(this.get_tile_id(this.coordinate.x, this.coordinate.y));
+            if (c) {
+                c.classList.add("current");
+            } 
+
+        };
+        this.initialize_viewport = function () {
+            var topleft = this.get_topleft();
+            this.generate_rows(topleft, h, w);
+            var that = this;
+            document.onkeypress = function (event) {
+                if (event.key === "W" || event.key === "w") {
+                    that.shift_viewport_vertically(1);
+                } else if (event.key === "S" || event.key === "s") {
+                    that.shift_viewport_vertically(-1);
+                }
+            }
+        };
+        this.shift_viewport_vertically = function (distance) {
+            this.coordinate.y += distance;
+            if(distance > 0){
+                console.log("moving up");
+                for (var i = 0; i < distance; i++) {
+                    el.removeChild(el.lastChild);
+                }
+
+                var tl = this.get_topleft();
+                this.generate_rows(tl, distance, w, 0);
+            }
         };
     }
 
