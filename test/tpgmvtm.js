3fe83a45019a887be2fa987c60c03d8ca66fb186
Ken Zhou <mailofrealcev@gmail.com>
2019-02-16 19:50:15 -0500

    wip

--- test/tpgmvtm.js
+++ test/tpgmvtm.js
@@ -13,6 +13,7 @@
                     var sx = ((left + x) < 0 ? "n" : "") + String(left + x);
                     td.id = "c_" + sx + "_" + sy;
                     td.innerText = String(left + x) + "," + String(top - y);
+                    td.style.background = "rgb(" + Math.abs(left + x) * 8 % 256 + ',' + Math.abs(top - y) * 8 % 256 + ', 0)';
                     if (this.coordinate.x === left + x && this.coordinate.y == top - y) {
                         td.classList.add("current");
                     }
@@ -22,12 +23,20 @@
                     el.appendChild(tr);
             }
         };
-        this.move = function () {
-            
-        }
+        this.shift_viewport_vertically = function(is_up, distance){
+            if(is_up){
+                this.coordinate += distance;
+            }
+            else{
+                this.coordinate -= distance;
+            }
+            /*
+            //to-do: remove bottom <tr>'s and insert new <tr>'s at the top            
+            */
+        };
     }
 
     var game = new Game();
     game.initialize_viewport(w, h);
 
-})(document.querySelector("#tvp"), 5, 5);
+})(document.querySelector("#tvp"), 25, 25);
