5b76fb42715c0243231e8c2165b034cdde68f1a0
Ken Zhou <mailofrealcev@gmail.com>
2019-03-28 21:06:18 -0400

    weird dragging the dude around thing

--- index.js
+++ index.js
@@ -455,12 +455,19 @@
                     skips++;
                 }
             };
-            document.onkeydown = function (e) {
+            document.onkeydown = function(e) {
                 if (e.key === " ") {
                     $('#dialogue-continue').addClass('active');
 
                 }
-            }
+            };
+            $('#dialogue-continue').click(function() {
+                $('body').css({
+                    '--dialogue-display': 'none'
+                });
+                $('#dialogue-continue').removeClass('active');
+                clearInterval(global.GameObject.dialogueInterval);
+            });
             var a = new Chunk(this, 25, {
                 x: this.get_bottomleft().x,
                 y: this.get_bottomleft().y
@@ -641,7 +648,52 @@
             $('body').css({
                 '--dialogue-display': 'block'
             });
+        };
+
+        function dragElement(elmnt) {
+            var pos1 = 0,
+                pos2 = 0,
+                pos3 = 0,
+                pos4 = 0;
+            if (document.getElementById(elmnt.id + "header")) {
+                /* if present, the header is where you move the DIV from:*/
+                document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+            } else {
+                /* otherwise, move the DIV from anywhere inside the DIV:*/
+                elmnt.onmousedown = dragMouseDown;
+            }
+
+            function dragMouseDown(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // get the mouse cursor position at startup:
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                document.onmouseup = closeDragElement;
+                // call a function whenever the cursor moves:
+                document.onmousemove = elementDrag;
+            }
+
+            function elementDrag(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX;
+                pos2 = pos4 - e.clientY;
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                // set the element's new position:
+                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            }
+
+            function closeDragElement() {
+                /* stop moving when mouse button is released:*/
+                document.onmouseup = null;
+                document.onmousemove = null;
+            }
         }
+        dragElement(qs('#dialogue-character'));
         this.save = function() {
             var savedGame = new GameSave();
             for (var k in that) {
